fcf0f3a9bc90961c515b74dfc130b770e718f2ad
Avi Koen <akoen@myseneca.ca>
2024-05-24 00:39:47 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Avi Koen
+# Author ID: YourSenecaID
+# Date Created: 2024/05/24
+
+import sys
+
+timer = int(sys.argv[1])
+while timer > 0:
+    print(timer)
+    timer -= 1
+print('blast off!')
+
